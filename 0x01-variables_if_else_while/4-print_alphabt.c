124f59e34620381a622e039cdb1c1555e822f0f0
NoughtG <hod.datubo@gmail.com>
2023-03-17 14:12:37 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Alphabet SOup
+ * Return: 0
+ */
+int main(void)
+{
+	char missingLetter[24] = "abcdfghijklmnoprstuvwxyz";
+	int a;
+
+	for (a = 0; a < 24; a++)
+	{
+		putchar(missingLetter[a]);
+	}
+	putchar('\n');
+	return 0;
+}

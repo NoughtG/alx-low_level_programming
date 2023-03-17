1bd42f140ae96ea93e113a6da5fd96570d84b3e6
NoughtG <hod.datubo@gmail.com>
2023-03-17 13:29:55 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print_alphabet.c
+ * Return: 0
+ */
+int main(void)
+{
+	char letter[26] = 'abcdefghijklmnopqrstuvwxyz';
+	int a;
+
+	for (a = 0; a < 26; a++)
+	{
+		putchar(letter[a]);
+	}
+	putchar('\n');
+	return (0);
+}

bb04e419905f6663f01c4114ede39e8c83cb1b6c
NoughtG <hod.datubo@gmail.com>
2023-03-23 13:45:38 -0700

    message

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_most_numbers
+ * Return: 0 to 9
+ */
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		if (x == 2 || x == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(x + '0');
+		}
+		_putchar('\n');
+	}
+}

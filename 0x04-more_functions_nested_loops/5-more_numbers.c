51cc1c32cfd11d98138a3ceef56425b4cdd6dca9
NoughtG <hod.datubo@gmail.com>
2023-03-24 11:51:35 -0700

    m

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers
+ * Return: ten lines of 0 to 14
+ */
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y < 0)
+			{
+				_putchar((y / 10) + '0');
+			}
+			_putchar((y % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
